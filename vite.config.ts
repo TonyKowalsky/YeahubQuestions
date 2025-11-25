7f049ef766c1ddb0774b0e515dda30c21fd9e0f9
TonyKowalsky <antonkovalev@DESKTOP-9PK03FM.localdomain>
2025-11-25 16:56:15 +0500

    Fix Vercel proxy setup

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
@@ -14,7 +13,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api": {
-        target: "https://api.yeatwork.ru/",
+        target: "https://api.yeatwork.ru",
         changeOrigin: true,
         rewrite: path => path.replace(/^\/api/, ""),
         secure: true,
